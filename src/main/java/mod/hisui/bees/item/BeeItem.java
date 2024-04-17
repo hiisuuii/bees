89fee74b061492de29642fc798ecf42bcdaf4ad2
hiisuuii <dogglez7777@gmail.com>
2024-04-17 15:06:24 -0600

    bee type handling

--- src/main/java/mod/hisui/bees/item/BeeItem.java
+++ src/main/java/mod/hisui/bees/item/BeeItem.java
@@ -1,10 +1,17 @@
 package mod.hisui.bees.item;
 
+import mod.hisui.bees.registry.BeeTypeRegistry;
 import mod.hisui.bees.types.BeeType;
+import mod.hisui.bees.types.BeeTypeDefinitions;
+import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
 import net.minecraft.item.Item;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 
 public class BeeItem extends Item {
     private final Identifier beeType;
@@ -14,7 +21,8 @@ public class BeeItem extends Item {
         this.beeType = type;
     }
 
-    public Identifier getType() {
-        return beeType;
+    public BeeType getType(World world) {
+        return world.getRegistryManager().get(BeeTypeRegistry.BEE_TYPE_KEY)
+                .getOrEmpty(this.beeType).orElse(BeeTypeDefinitions.FOREST);
     }
 }
